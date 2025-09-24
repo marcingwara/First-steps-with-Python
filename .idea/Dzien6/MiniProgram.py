1fa7d404840b56c9caeea6cf87a12f2febe27d1a
marcingwara <marcingwara@gmx.de>
2025-09-24 13:26:10 +0200

    Day 6(try,except)

--- .idea/Dzien6/MiniProgram.py
+++ .idea/Dzien6/MiniProgram.py
@@ -12,29 +12,46 @@ while True:
     print("2: Obliczanie pola")
     print("3: Obliczanie sredniej trzech liczb")
     print("4: Zakoncz")
+    try:
+        dzialanie = int (input("Wybierz rodzaj działania od 1-4: "))
+    except ValueError :
+        print("To nie jest liczba, sprobuj ponownie")
+        print()
+        continue
 
-    dzialanie = int (input("Wybierz rodzaj działania od 1-4: "))
 
     if dzialanie == 1:
-        a = float(input("Podaj pierwsza liczbe: "))
-        b = float(input("Podaj druga liczbe: "))
-        print("Suma liczb to: ", dodawanie(a,b))
-        print()
+        try:
+            a = float(input("Podaj pierwsza liczbe: "))
+            b = float(input("Podaj druga liczbe: "))
+            print("Suma liczb to: ", dodawanie(a,b))
+            print()
+        except ValueError:
+            print("Musisz podac liczbe!!!")
+            print()
 
     elif dzialanie == 2:
-
-        a = float(input("Podaj dlugosc pierwszego boku prostokąta: "))
-        b = float(input("Podaj dlugosc drugiego boku prostokąta: "))
-        print("Pole prostokąta to: ", pole(a,b))
-        print()
+        try:
+            a = float(input("Podaj dlugosc pierwszego boku prostokąta: "))
+            b = float(input("Podaj dlugosc drugiego boku prostokąta: "))
+            print("Pole prostokąta to: ", pole(a,b))
+            print()
+        except ValueError:
+            print("Musisz podac liczeb!!!")
+            print()
 
     elif dzialanie == 3:
-        a = float(input("Podaj pierwsza liczbe: "))
-        b = float(input("Podaj druga liczbe: "))
-        c = float(input("Podaj trzecia liczbe: "))
-        print("Srednia z trzech liczb to: ", srednia(a,b,c))
-        print()
+        try:
+            a = float(input("Podaj pierwsza liczbe: "))
+            b = float(input("Podaj druga liczbe: "))
+            c = float(input("Podaj trzecia liczbe: "))
+            print("Srednia z trzech liczb to: ", srednia(a,b,c))
+            print()
+        except ValueError:
+            print("Musisz podac liczbe!!!")
+            print()
     elif dzialanie == 4:
+        print("Koniec programu, do zobaczenia")
         break
     else:
         print("Nie wlasciwe dzialnie")
